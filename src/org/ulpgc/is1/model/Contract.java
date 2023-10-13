b3ed46fc759002e6d15bf453e9ffeac80b11c835
Nicolas <nicolastrujillloestevez@gmail.com>
2023-10-13 15:44:30 +0100

    third commit, Task and Contract ended

--- src/org/ulpgc/is1/model/Contract.java
+++ src/org/ulpgc/is1/model/Contract.java
@@ -1,4 +1,38 @@
 package org.ulpgc.is1.model;
 
+import java.util.Date;
+
 public class Contract {
+    private Date start;
+    private Date end;
+    private int budget;
+    public Contract(Date start, Date end, int budget){
+        this.start = start;
+        this.end = end;
+        this.budget = budget;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
 }
