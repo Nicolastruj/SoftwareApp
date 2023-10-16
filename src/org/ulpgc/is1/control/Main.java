954ccf85a11b6fb00f47e62fabb3ffe9d3fdb950
Sesion2 <luispereraperez@gmail.com>
2023-10-16 17:24:44 +0100

    10th commit, added Main

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -1,7 +1,35 @@
 package org.ulpgc.is1.control;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+    // i Init. Crear dos clientes. (*) En el caso que el cliente tenga un número de teléfono no válido se almacenará el valor: “XXXX”.
+
+
+    // ii. Init. Crear dos empleados.
+
+
+    // iii. Init. Crear un proyecto vinculado al primer cliente. El primer empleado será desarrollador en el proyecto. El segundo será el manager.
+
+
+    // iv. Init. Crear dos tareas vinculadas al proyecto anterior.
+
+
+    // v. Imprimir por pantalla los datos del primer cliente.
+
+
+    // vi. Imprimir por pantalla los datos del segundo empleado.
+
+
+    // vii. Imprimir por pantalla los datos del proyecto del primer cliente.
+
+
+    // viii. Imprimir por pantalla los datos de las tareas del proyecto.
+
+
+    // ix. Borrar el segundo cliente.
+
+
+    // x. Imprimir por pantalla el número de clientes
+
+
 }
