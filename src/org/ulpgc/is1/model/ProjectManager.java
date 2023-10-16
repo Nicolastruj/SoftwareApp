7d1d24bec0b76c81277609e60454bb535b1e2314
Sesion2 <luispereraperez@gmail.com>
2023-10-16 17:20:00 +0100

    9th commit, added ProjectManager

--- src/org/ulpgc/is1/model/ProjectManager.java
+++ src/org/ulpgc/is1/model/ProjectManager.java
@@ -1,4 +1,52 @@
 package org.ulpgc.is1.model;
 
+import java.time.Period;
+import java.util.ArrayList;
+
 public class ProjectManager {
+
+    private ArrayList<Employee> employeeList;
+    private ArrayList<Customer> customerList;
+
+    public ProjectManager(ArrayList<Employee> employeeList, ArrayList<Customer> customerList) {
+        this.employeeList = employeeList;
+        this.customerList = customerList;
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    public ArrayList<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(ArrayList<Customer> customerList) {
+        this.customerList = customerList;
+    }
+
+    public void addCustomer(Customer customer){
+        customerList.add(customer);
+    }
+
+    public void addEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public Customer getCustomer(int index){
+        return customerList.get(index);
+    }
+
+    public Employee getEmployee(int index){
+        return employeeList.get(index);
+    }
+
+    public Project project(){
+        return project();
+    }
+
 }
