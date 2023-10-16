3ef021e2f7b9a6265e37ab9d6f6347ee2f1814b0
Sesion2 <luispereraperez@gmail.com>
2023-10-16 17:40:53 +0100

    10th commit, added Main

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -1,15 +1,19 @@
 package org.ulpgc.is1.control;
 
 import org.ulpgc.is1.model.Customer;
+import org.ulpgc.is1.model.Employee;
+import org.ulpgc.is1.model.Project;
+import org.ulpgc.is1.model.Task;
 
 public class Main {
 
     // i Init. Crear dos clientes. (*) En el caso que el cliente tenga un número de teléfono no válido se almacenará el valor: “XXXX”.
     Customer customer1 = new Customer("Luis", "Perera", "978654321");
-
+    Customer customer2 = new Customer("Nicolas", "Trujillo", "123456798");
 
     // ii. Init. Crear dos empleados.
-
+    Employee employee1 = new Employee("Jorge", "jorge@gmail.com");
+    Employee employee2 = new Employee("Ines", "ines@gmail.com");
 
     // iii. Init. Crear un proyecto vinculado al primer cliente. El primer empleado será desarrollador en el proyecto. El segundo será el manager.
 
--- src/org/ulpgc/is1/model/Employee.java
+++ src/org/ulpgc/is1/model/Employee.java
@@ -10,13 +10,10 @@ public class Employee {
     private ArrayList<Project> projectListDeveloper;
     private ArrayList<Project> projectListManager;
 
-    public Employee(String name, String email, ArrayList<Task> tasksList, ArrayList<Project> projectListDeveloper, ArrayList<Project> projectListManager) {
+    public Employee(String name, String email) {
         this.name = name;
         this.email = email;
-        this.tasksList = tasksList;
-        this.projectListDeveloper = projectListDeveloper;
-        this.projectListManager = projectListManager;
-    }
+        }
 
     public void addTask(String name, String description, Date start, Date end, TaskType type){
         Task task = new Task(name, description, start, end, type);
@@ -37,30 +34,6 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public ArrayList<Task> getTasksList() {
-        return tasksList;
-    }
-
-    public void setTasksList(ArrayList<Task> tasksList) {
-        this.tasksList = tasksList;
-    }
-
-    public ArrayList<Project> getProjectListDeveloper() {
-        return projectListDeveloper;
-    }
-
-    public void setProjectListDeveloper(ArrayList<Project> projectListDeveloper) {
-        this.projectListDeveloper = projectListDeveloper;
-    }
-
-    public ArrayList<Project> getProjectListManager() {
-        return projectListManager;
-    }
-
-    public void setProjectListManager(ArrayList<Project> projectListManager) {
-        this.projectListManager = projectListManager;
     }
 }
 
